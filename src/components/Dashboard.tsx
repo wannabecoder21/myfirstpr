f5d661a22e5d3ed8e3423773734da47782686867
Mukku <mukarram.ali89@gmail.com>
2020-10-15 00:24:13 +0200

    feat: loads dashboard with tiles

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: "rgba(255, 255, 255, 0.54)",
+    },
+  })
+);
+
+const tileData = [
+  {
+    img: "/contributors/mukarramali.jpg",
+    title: "Image",
+    author: "author",
+  },
+];
+export default function Dashboard() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+          <ListSubheader component="div">
+            HacktoberFest India 2020
+          </ListSubheader>
+        </GridListTile>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>by: {tile.author}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
