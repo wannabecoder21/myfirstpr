df8785591f7ac92070170d00b1f21a11a143e0b9
Mukku <mukarram.ali89@gmail.com>
2020-10-15 02:12:06 +0200

    feat: refactor cards

---
+++ src/components/Contributor.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+
+interface Contributor {
+  avatar?: string;
+  city?: string;
+  college?: string;
+  github: string;
+  name: string;
+  organization?: string;
+  quote: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    cardRoot: {
+      display: "flex",
+    },
+    cardDetails: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    cardContent: {
+      flex: "1 0 auto",
+    },
+    cardCover: {
+      width: 151,
+    },
+  })
+);
+
+export default function ContributorCard(props: Contributor) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.cardRoot}>
+      <div className={classes.cardDetails}>
+        <CardContent className={classes.cardContent}>
+          <Typography component="h5" variant="h5">
+            {props.name}
+          </Typography>
+          {props.organization && (
+            <Typography variant="subtitle1" color="textSecondary">
+              {props.organization}
+            </Typography>
+          )}
+          {props.college && (
+            <Typography variant="subtitle1" color="textSecondary">
+              {props.college}
+            </Typography>
+          )}
+          {props.quote && (
+            <Typography variant="subtitle2" color="textSecondary">
+              {`- "${props.quote}"`}
+            </Typography>
+          )}
+        </CardContent>
+      </div>
+      <CardMedia
+        className={classes.cardCover}
+        image={`/contributors/${props.avatar || "default.png"}`}
+        title={props.name}
+      />
+    </Card>
+  );
+}
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,52 +1,34 @@
 import React from "react";
-import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import Contributors from "./contributors.json";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import contributors from "./contributors.json";
+import ContributorCard from "./Contributor";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: "flex",
-      flexWrap: "wrap",
-      justifyContent: "space-around",
-      overflow: "hidden",
-      backgroundColor: theme.palette.background.paper,
+      width: "100%",
     },
-    gridList: {
-      width: 500,
-      height: 450,
+    inline: {
+      display: "inline",
     },
-    icon: {
-      color: "rgba(255, 255, 255, 0.54)",
+    listItem: {
+      justifyContent: "center",
     },
   })
 );
 
 export default function Dashboard() {
   const classes = useStyles();
+
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
-          <ListSubheader component="div">
-            HacktoberFest India 2020
-          </ListSubheader>
-        </GridListTile>
-        {Contributors.map((contributor) => (
-          <GridListTile key={contributor.github}>
-            <img
-              src={`/contributors/${contributor.avatar}`}
-              alt={contributor.name}
-            />
-            <GridListTileBar
-              title={contributor.name}
-              subtitle={contributor.college}
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    <List className={classes.root}>
+      {contributors.map((contributor) => (
+        <ListItem className={classes.listItem}>
+          <ContributorCard {...contributor} />
+        </ListItem>
+      ))}
+    </List>
   );
 }
