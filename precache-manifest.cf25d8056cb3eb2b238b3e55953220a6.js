c60b340b4c887cd89e04c5a7b7196fa472c6a24d
Mukku <mukarram.ali89@gmail.com>
2020-10-15 02:31:51 +0200

    Updates

---
+++ precache-manifest.cf25d8056cb3eb2b238b3e55953220a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb383dee0071b6703b3583f70a5994b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf6c8bded80347b4369",
+    "url": "/static/css/main.22679b2b.chunk.css"
+  },
+  {
+    "revision": "42d993c3273f560e70b2",
+    "url": "/static/js/2.e9b7a8d6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.e9b7a8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf6c8bded80347b4369",
+    "url": "/static/js/main.71c7ad96.chunk.js"
+  },
+  {
+    "revision": "9e864b1c21a7a3582faa",
+    "url": "/static/js/runtime-main.901f5971.js"
+  }
+]);
